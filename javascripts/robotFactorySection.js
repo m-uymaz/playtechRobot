739ee1c7336278165b3e90b4cf687e2f0c070c95
Mihail Uymaz <michael.m.uymaz@gmail.com>
2022-05-04 22:37:19 +0300

    dom creation, valdation, carousel

---
+++ javascripts/robotFactorySection.js
@@ -0,0 +1,139 @@
+const buildSection = () => {
+    const section = document.createElement("section");
+    section.classList.add("factory-section");
+    const basicRobot = document.createElement("div");
+    basicRobot.setAttribute("id", "basicRobot");
+    const h3 = document.createElement("h3");
+    h3.textContent = "Basic Robot";
+    basicRobot.appendChild(h3);
+
+    const contentWrapper = document.createElement("div");
+    contentWrapper.classList.add("content-wrapper");
+    const robotContainer = document.createElement("div");
+    robotContainer.setAttribute("id", "robotContainer");
+    const robotFriend = document.createElement("div");
+    robotFriend.classList.add("robotFriend");
+
+    // ROBOT HEAD
+    const head = document.createElement("div");
+    head.setAttribute("id", "head");
+    const eyes = document.createElement("div");
+    eyes.setAttribute("id", "eyes");
+    const eyeLeft = document.createElement("div");
+    eyeLeft.classList.add("eye");
+    const eyeRight = document.createElement("div");
+    eyeRight.classList.add("eye");
+    const mouth = document.createElement("div");
+    mouth.classList.add("mouth");
+
+    eyes.appendChild(eyeLeft);
+    eyes.appendChild(eyeRight);
+    head.appendChild(eyes);
+    head.appendChild(mouth);
+
+    // ROBOT BODY AND NAME
+    const body = document.createElement("div");
+    body.setAttribute("id", "body");
+    const leftHand = document.createElement("div");
+    leftHand.classList.add("hand", "leftHand");
+    const torso = document.createElement("div");
+    torso.classList.add("torso");
+    const rightHand = document.createElement("div");
+    rightHand.classList.add("hand", "rightHand");
+    const leftLeg = document.createElement("div");
+    leftLeg.classList.add("leg", "leftLeg");
+    const rightLeg = document.createElement("div");
+    rightLeg.classList.add("leg", "rightLeg");
+
+    body.appendChild(leftHand);
+    body.appendChild(torso);
+    body.appendChild(rightHand);
+    body.appendChild(leftLeg);
+    body.appendChild(rightLeg);
+
+    const robotNameContainer = document.createElement("div");
+    robotNameContainer.classList.add("robotNameContainer");
+
+    robotFriend.appendChild(head);
+    robotFriend.appendChild(body);
+
+
+    // PARAGRAPHS
+    const paragraphs = document.createElement("div");
+    paragraphs.setAttribute("id", "paragraphs");
+
+    // P1
+    const p1Div = document.createElement("div");
+    p1Div.classList.add("paragraph", "p1", "tooltip");
+    const p1Span = document.createElement("span");
+    p1Span.classList.add("paragraphTooltip");
+    p1Span.textContent =
+        "Paragraph text here. Text-align on left, 3% space from right border." +
+        "Text is only 2 lines and ends with three dots... because it too long." +
+        "On hover full text is shown as tooltip. First letter is bold.";
+    const p1 = document.createElement("p");
+    p1.classList.add("p", "pTransition1");
+    const p1SpanBold = document.createElement("span");
+    p1SpanBold.classList.add("bold");
+    p1SpanBold.textContent = "Paragraph ";
+    p1.textContent =
+        "text here. Text-align on left, 3% space from right border." +
+        "Text is only 2 lines and ends with three dots... because it too long." +
+        "On hover full text is shown as tooltip. First letter is bold.";
+    p1.prepend(p1SpanBold);
+    p1Div.appendChild(p1Span);
+    p1Div.appendChild(p1);
+
+    // P2
+    const p2Div = document.createElement("div");
+    p2Div.classList.add("paragraph", "p2", "tooltip");
+
+    const p2Span = document.createElement("span");
+    p2Span.textContent =
+        "Paragraph text here. Text justified, 6% space from right border." +
+        "Text is only 2 lines and ends with three dots... because it too long." +
+        "On hover full text is shown. First word is Italic.";
+    p2Span.classList.add("paragraphTooltip");
+    const p2 = document.createElement("p");
+    p2.classList.add("p", "pTransition2");
+    p2.textContent =
+        "text here. Text justified, 6% space from right border." +
+        "Text is only 2 lines and ends with three dots... because it too long." +
+        "On hover full text is shown. First word is Italic.";
+    const p2SpanItalic = document.createElement("span");
+    p2SpanItalic.classList.add("italic");
+    p2SpanItalic.textContent = "Paragraph ";
+    p2.prepend(p2SpanItalic);
+
+    p2Div.appendChild(p2Span);
+    p2Div.appendChild(p2);
+
+    // P3
+    const p3Div = document.createElement("div");
+    p3Div.classList.add("paragraph", "p3");
+    const p3Img = document.createElement("img");
+    p3Img.setAttribute("src", "good_luck.jpg");
+    const p3 = document.createElement("p");
+    p3.classList.add("p", "pTransition3");
+    p3.textContent = "Paragraph text here. Text-align in left, 9% from right border. Image on left side.";
+
+    p3Div.appendChild(p3Img);
+    p3Div.appendChild(p3);
+
+    // Last appendments
+
+    paragraphs.appendChild(p1Div);
+    paragraphs.appendChild(p2Div);
+    paragraphs.appendChild(p3Div);
+
+    robotContainer.appendChild(robotFriend);
+    robotContainer.appendChild(robotNameContainer);
+
+    contentWrapper.appendChild(robotContainer);
+    contentWrapper.appendChild(paragraphs);
+
+    section.appendChild(basicRobot);
+    section.appendChild(contentWrapper);
+
+    return section;
+}
---
+++ javascripts/robotFormAndCreation.js
@@ -0,0 +1,159 @@
+//Robot Storage
+const robotModels = [];
+
+const form = document.querySelector("#robot-form");
+const phrase = form.querySelector("input[name='phrase']");
+form.addEventListener("submit", formSubmit);
+
+//Checkbox for "Phrase" input
+const onCheckboxChange = () => {
+    const talkCheckbox = form.querySelector("input[name='canTalk']");
+    const phrase = form.querySelector("input[name='phrase']");
+    phrase.disabled = !talkCheckbox.checked;
+    if (!talkCheckbox.checked) {
+        phrase.value = "";
+    };
+};
+
+//Form submission and validation
+function formSubmit(event) {
+    const talkCheckbox = form.querySelector("input[name='canTalk']");
+
+    const name = form.querySelector("#robotName");
+    const type = form.querySelector("#selectType");
+    const color = form.querySelector("#selectColor");
+
+    const phrase = form.querySelector("input[name='phrase']");
+    clearErrorMessages();
+    event.preventDefault();
+    if (!name.value || !type.value || !color.value || !phrase.disabled && phrase.value === "") {
+        if (!name.value) {
+            const spanErrorName = document.querySelector(".errorName");
+            const nameErrorMessage = "Please enter a name";
+            showError(name, nameErrorMessage, spanErrorName);
+        }
+        if (!type.value) {
+            const spanErrorType = document.querySelector(".errorType");
+            const typeErrorMessage = "Select a type";
+            showError(type, typeErrorMessage, spanErrorType);
+        }
+        if (!color.value) {
+            const spanErrorColor = document.querySelector(".errorColor");
+            const colorErrorMessage = "Chose a color";
+            showError(color, colorErrorMessage, spanErrorColor);
+        }
+        if (!phrase.disabled && phrase.value === "") {
+            const spanErrorPhrase = document.querySelector(".errorPhrase");
+            const phraseErrorMessage = "Please write a phrase";
+            showError(phrase, phraseErrorMessage, spanErrorPhrase);
+        };
+        return false;
+    }
+    if (phrase.disabled) {
+        robotModels.push({
+            name: name.value,
+            type: type.value,
+            color: color.value,
+        });
+        createRobot(name, type, color, phrase);
+    } else {
+        robotModels.push({
+            name: name.value,
+            type: type.value,
+            color: color.value,
+            phrase: phrase.value,
+        });
+        createRobot(name, type, color, phrase);
+        phrase.value = "";
+    };
+    clearErrorMessages();
+    console.log(robotModels);
+    form.reset();
+    phrase.disabled = !talkCheckbox.checked;
+};
+
+//Shows the error
+const showError = (input, errorMessage, spanError) => {
+    if (input.value === "") {
+        spanError.textContent = errorMessage;
+    };
+};
+
+//Clears messages on a submit or a submit try after an error
+const clearErrorMessages = () => {
+    const allErrorSpans = document.querySelectorAll(".error");
+    allErrorSpans.forEach(el => el.textContent = "");
+};
+
+//Visual creation of Robots
+const createRobot = (name, type, color, phrase) => {
+    const body = document.querySelector("body");
+    const slide1 = buildSection();
+    const slide2 = body.querySelector("#slide-2");
+
+    const robotFriend = slide1.querySelector(".robotFriend");
+
+    //Checkboxes
+    const jumpCheckbox = form.querySelector("input[name='canJump']");
+    const talkCheckbox = form.querySelector("input[name='canTalk']");
+    const blinkCheckbox = form.querySelector("input[name='canBlink']");
+
+
+    //Robot body
+    const eye = slide1.querySelector("#eyes div:nth-of-type(2)");
+    const torso = slide1.querySelector(".torso");
+    const mouth = slide1.querySelector(".mouth");
+    const leftHand = slide1.querySelector(".leftHand");
+    const rightHand = slide1.querySelector(".rightHand");
+    const leftLeg = slide1.querySelector(".leftLeg");
+    const rightLeg = slide1.querySelector(".rightLeg");
+
+    const robotNameContainer = slide1.querySelector(".robotNameContainer");
+    const ribon = slide1.querySelector("#basicRobot > h3");
+
+    if (type.value === "male") {
+        torso.classList.add("torso-male");
+        leftHand.classList.add("leftHand-male");
+        rightHand.classList.add("rightHand-male");
+        const torsoMale = slide1.querySelector(".torso-male");
+        torsoMale.style.borderTop = `100px solid ${color.value}`;
+        ribon.textContent = "Male Robot";
+    } else {
+        torso.classList.add("torso-female");
+        leftHand.classList.add("leftHand-female");
+        rightHand.classList.add("rightHand-female");
+        const torsoFemale = slide1.querySelector(".torso-female");
+        torsoFemale.style.borderBottom = `100px solid ${color.value}`;
+        ribon.textContent = "Female Robot";
+    };
+
+    // Adding animations depending on checked checkbox
+    if (jumpCheckbox.checked) {
+        robotFriend.classList.add("wholeRobotJump");
+        leftLeg.classList.add("legsJump");
+        rightLeg.classList.add("legsJump");
+    };
+    if (talkCheckbox.checked) {
+        const robotContainer = slide1.querySelector("#robotContainer");
+        const talkingBubble = document.createElement("div");
+        talkingBubble.classList.add("bubble", "bubble-bottom-left");
+        
+        const spanBubble = document.createElement("span");
+        spanBubble.classList.add("spanBubble");
+        spanBubble.textContent = phrase.value;
+        talkingBubble.appendChild(spanBubble);
+        robotContainer.appendChild(talkingBubble);
+        mouth.classList.add("talk");
+    };
+    if (blinkCheckbox.checked) {
+        eye.classList.add("blink");
+    };
+    robotNameContainer.textContent = name.value;
+    const carouselContainer = body.querySelector(".ct");
+    carouselContainer.classList.add("carousel-container");
+    const slideButtons = body.querySelector(".slide-buttons");
+    slideButtons.style.display = "flex";
+    const carousel = body.querySelector(".carousel");
+
+    carousel.appendChild(slide1);
+};
